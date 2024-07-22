99ef60fdd3b51986642edb4209f63a0a52e65a8b
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-07-22 21:12:20 +0100

    update options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,3 +7,11 @@ local opt = vim.opt
 opt.colorcolumn = "81,101" -- Show column colors.
 opt.wrap = true -- Enable text wrap
 opt.scrolloff = 0 -- Lines of context
+
+opt.number = true -- Print line number
+opt.relativenumber = true -- Relative line numbers
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.colorcolumn = '80'
+opt.cursorline = true
+
+vim.g.mapleader = "\\" -- Set leader key to m
