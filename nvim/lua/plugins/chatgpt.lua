187ad3e9bc4dbdfbcb3a6f57ce0dc2d51acd5984
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-07-22 21:12:39 +0100

    update plugin chatgpt.lua

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -10,10 +10,19 @@ return {
   config = function()
     require("chatgpt").setup({
       -- api_key_cmd = "$OPENAI_TERMINAL",
-      -- api_key_cmd = "echo $OPENAI_TERMINAL",
+      api_key_cmd = "echo $OPENAI_TERMINAL",
       -- NOTE: Don't use as it keep asking to confirm.
       -- api_key_cmd = "pass show openai/terminal",
       -- api_key_cmd = "op read op://personal/Openai/terminal --no-newline",
+      openai_params = {
+        model = "gpt-3.5-turbo-16k",
+        max_tokens = 100,
+        temperature = 0.5,
+        top_p = 1,
+        frequency_penalty = 0,
+        presence_penalty = 0,
+        stop = "\n",
+      },
     })
   end,
   dependencies = {
