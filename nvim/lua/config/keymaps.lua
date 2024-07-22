c6bf7351c7d256da05bd6520c29b48e84c94b92f
Rodrigo Martins <rrmartinsjg@gmail.com>
2024-07-22 21:12:07 +0100

    update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local opts = { noremap = true, silent = true }
+
 -- You don't need this as you can use `<Leader><Tab>[` or `<Leader><Tab>]` instead!
 vim.keymap.set("n", "gb", "gT", { desc = "Go back one tab", noremap = true, silent = true })
 
@@ -16,3 +18,28 @@ vim.keymap.set("n", "<leader><tab>+", ":tabm+<CR>", { desc = "Move tab to the ri
 vim.keymap.set("n", "<leader>ttn", ":TestNearest<CR>", { desc = "Test nearest", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>ttf", ":TestFile<CR>", { desc = "Test file", noremap = true, silent = true })
 vim.keymap.set("n", "<leader>ttl", ":TestLast<CR>", { desc = "Test last", noremap = true, silent = true })
+
+vim.keymap.set('n', 'gd', '<Plug>(coc-definition)', {})
+vim.keymap.set('n', 'gr', '<Plug>(coc-references)', {})
+
+vim.keymap.set('n', '<leader>nt', ':NERDTreeToggle <CR><esc>', opts)
+
+vim.keymap.set('n', '<C-e>', ':e<cr>', opts)
+vim.keymap.set('n', '<C-o>', ':noh<cr><esc>', opts)
+vim.keymap.set('n', '<C-s>', ':w<cr><esc>', opts)
+vim.keymap.set('n', '<C-Q>', ':qa<cr><esc>', opts)
+vim.keymap.set('n', '<C-q>', ':q<cr><esc>', opts)
+
+vim.keymap.set('n', '<Tab>', ':tabnext<CR>', opts)
+vim.keymap.set('n', '<S-Tab>', ':tabprevious<CR>', opts)
+
+-- function ReloadConfig()
+--   for name,_ in pairs(package.loaded) do
+--     if name:match('^user') or name:match('^plugins') then
+--       package.loaded[name] = nil
+--     end
+--   end
+--   dofile(vim.fn.stdpath('config') .. '/init.lua')
+-- end
+
+-- vim.api.nvim_set_keymap('n', '<leader>r', ':lua ReloadConfig()<CR>', { noremap = true, silent = true })
