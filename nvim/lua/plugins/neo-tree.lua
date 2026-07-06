e6163ed532e29115951c47d468254c3b06351a96
Rodrigo Martins <rrmartinsjg@gmail.com>
2026-07-06 11:55:38 -0300

    feat(nvim): disable neo-tree in favor of snacks explorer

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,31 +1,6 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts)
-      opts = opts or {}
-      opts.use_default_mappings = true
-      opts.default_component_configs = opts.default_component_configs or {}
-      opts.default_component_configs.icon = {
-        folder_closed = "+",
-        folder_open = "-",
-        folder_empty = ".",
-        default = " ", -- files
-      }
-      opts.default_component_configs.git_status = {
-        symbols = {
-          added = "+",
-          modified = "~",
-          deleted = "-",
-          renamed = ">",
-          untracked = "?",
-          ignored = "!",
-          unstaged = "*",
-          staged = "=",
-          conflict = "x",
-        },
-      }
-      opts.enable_diagnostics = false
-      return opts
-    end,
+    enabled = false,
   },
 }
